231349e34ffdd3ad439bb8d46322b34d7d04774e
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-07-10 14:23:18 +0200

    changes

---
+++ src/app/components/content/AboutSection/AboutContent/AboutContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './AboutContent.module.css';
+
+export default function AboutContent({ containerRef }) {
+  return (
+    <section className={styles.Content}>
+      <div className={styles.FirstContent}>
+        <h1>Helloo</h1>
+      </div>
+    </section>
+  );
+}
--- src/app/components/content/AboutSection/AboutSection.js
+++ src/app/components/content/AboutSection/AboutSection.js
@@ -1,13 +1,42 @@
 import React, { useRef } from 'react';
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+import AboutContent from './AboutContent/AboutContent';
+
 import styles from './AboutSection.module.css';
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function AboutSection() {
-  const containerRef = useRef();
+  const containerRef = useRef(null);
+
+  useGSAP(
+    () => {
+      if (!containerRef.current) {
+        return;
+      }
+
+      ScrollTrigger.create({
+        trigger: containerRef.current,
+        start: 'top top',
+        end: 'bottom bottom',
+        pin: true,
+        pinSpacing: false,
+        anticipatePin: 1,
+      });
+
+      return () => {
+        ScrollTrigger.getAll().forEach((st) => st.kill());
+      };
+    },
+    { scope: containerRef }
+  );
+
   return (
     <section className={styles.Main} ref={containerRef}>
-      <div>
-        <h1>Helloo</h1>
-      </div>
+      <AboutContent containerRef={containerRef} />
     </section>
   );
 }
