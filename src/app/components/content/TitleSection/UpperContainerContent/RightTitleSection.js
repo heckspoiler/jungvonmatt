9ce74c6bf24f14c139f909d5145921907e58c3bc
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-07-10 11:24:52 +0200

    what the fuck

--- src/app/components/content/TitleSection/UpperContainerContent/RightTitleSection.js
+++ src/app/components/content/TitleSection/UpperContainerContent/RightTitleSection.js
@@ -27,36 +27,38 @@ export default function RightTitleSection({ styles, containerRef }) {
       const tl = gsap.timeline({ paused: false });
 
       tl.from(boxRef.current, {
+        lazy: false,
         y: 100,
         duration: 0.6,
         ease: 'circ.out',
         delay: 0.8,
       }).from(split.chars, {
+        lazy: false,
         y: 100,
         duration: 0.6,
         stagger: 0.02,
         ease: 'circ.out',
       });
 
-      const scrollTl = gsap.timeline({
-        scrollTrigger: {
-          trigger: boxRef.current,
-          start: 'top 42%',
-          end: 'bottom top',
-          pin: true,
-          toggleActions: 'play none none reverse',
-          scrub: 0.3,
-          // markers: true,
-          pinSpacing: false,
-        },
-      });
+      // const scrollTl = gsap.timeline({
+      //   scrollTrigger: {
+      //     trigger: boxRef.current,
+      //     start: 'top 42%',
+      //     end: 'bottom top',
+      //     pin: true,
+      //     toggleActions: 'play none none reverse',
+      //     scrub: 0.3,
+      //     // markers: true,
+      //     pinSpacing: false,
+      //   },
+      // });
 
-      scrollTl.to(boxRef.current, {
-        y: -600,
-        stagger: 0.03,
-        duration: 0.1,
-        ease: 'circ.in',
-      });
+      // scrollTl.to(boxRef.current, {
+      //   y: -600,
+      //   stagger: 0.03,
+      //   duration: 0.1,
+      //   ease: 'circ.in',
+      // });
     },
 
     { scope: containerRef }
