36fe3e7c5e6dc19d9c8a5cb3513c7e4f8db13050
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-07-11 13:03:15 +0200

    changes

--- src/app/components/content/AboutSection/AboutContent/Card/Card.js
+++ src/app/components/content/AboutSection/AboutContent/Card/Card.js
@@ -14,12 +14,25 @@ export default function Card() {
   const cardRef = useRef();
   const firstTitleRef = useRef();
   const textRef = useRef();
+  const firstBox = useRef();
+  const secondBox = useRef();
+  const thirdBox = useRef();
 
   useGSAP(
     () => {
-      if (!containerRef.current || !cardRef.current || !firstTitleRef.current) {
+      if (
+        !containerRef.current ||
+        !cardRef.current ||
+        !firstTitleRef.current ||
+        !textRef.current ||
+        !firstBox.current ||
+        !secondBox.current ||
+        !thirdBox.current
+      ) {
         return;
       }
+
+      const boxes = [firstBox.current, secondBox.current, thirdBox.current];
       const split = new SplitText(textRef.current, { type: 'chars' });
 
       ScrollTrigger.create({
@@ -53,6 +66,16 @@ export default function Card() {
               ease: 'power2.out',
             },
             '-=0.4'
+          )
+          .from(
+            boxes,
+            {
+              y: 100,
+              duration: 0.8,
+              stagger: 0.2,
+              ease: 'power2.out',
+            },
+            '-=0.4'
           ),
       });
     },
@@ -68,32 +91,39 @@ export default function Card() {
           </div>
         </div>
         <div className={styles.InfoContainer}>
-          <div className={styles.InfoLine}>
-            <div className={styles.InfoCell}>
-              <p>Name:</p>
-              <h2>Carlo Ettisberger</h2>
-            </div>
-            <div className={styles.InfoCell}>
-              <p>Birthday:</p>
-              <h2>20.12.1990</h2>
-            </div>
-          </div>
-          <div className={styles.InfoLine}>
-            <div className={styles.InfoCell}>
-              <p>Location:</p>
-              <h2>Zurich</h2>
-            </div>
-            <div className={styles.InfoCell}>
-              <p>Occupation:</p>
-              <h2>Web Developer</h2>
+          <div className={styles.OverflowContainer}>
+            <div className={styles.InfoLine} ref={firstBox}>
+              <div className={styles.InfoCell}>
+                <p>Name:</p>
+                <h2>Carlo Ettisberger</h2>
+              </div>
+
+              <div className={styles.InfoCell}>
+                <p>Birthday:</p>
+                <h2>20.12.1990</h2>
+              </div>
             </div>
           </div>
-          <div className={styles.InfoLine}>
-            <div className={styles.InfoCell}>
-              <h2>
-                For the past two years, I have been diligently self-educating to
-                become a dependable web developer and web designer.
-              </h2>
+          <div className={styles.OverflowContainer}>
+            <div className={styles.InfoLine} ref={secondBox}>
+              <div className={styles.InfoCell}>
+                <p>Location:</p>
+                <h2>Zurich</h2>
+              </div>
+              <div className={styles.InfoCell}>
+                <p>Occupation:</p>
+                <h2>Web Developer</h2>
+              </div>
+            </div>
+          </div>
+          <div className={styles.OverflowContainer}>
+            <div className={styles.InfoLine} ref={thirdBox}>
+              <div className={styles.InfoCell}>
+                <h2>
+                  For the past two years, I have been diligently self-educating
+                  to become a dependable web developer and web designer.
+                </h2>
+              </div>
             </div>
           </div>
         </div>
