0f4968b993918973aadefc7772daad1130b3c69d
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-07-12 19:13:40 +0200

    changes

--- src/app/components/content/WorkSection/WorkSection.js
+++ src/app/components/content/WorkSection/WorkSection.js
@@ -4,15 +4,44 @@ import React, { useRef } from 'react';
 import { gsap } from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from 'gsap/all';
-import SplitText from 'gsap/SplitText';
 import styles from './WorkSection.module.css';
 
-gsap.registerPlugin(ScrollTrigger, SplitText);
+gsap.registerPlugin(ScrollTrigger);
 
 export default function WorkSection({ isMobile }) {
   const containerRef = useRef();
+  const portalRef = useRef();
 
-  useGSAP(() => {}, { scope: containerRef });
+  useGSAP(
+    () => {
+      if (!containerRef.current || !portalRef.current) {
+        return;
+      }
 
-  return <div className={styles.Main} ref={containerRef}></div>;
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: 'top top',
+          end: 'bottom top',
+          markers: true,
+          pin: containerRef.current,
+          pinSpacing: false,
+          scrub: 0.1,
+        },
+      });
+
+      tl.from(portalRef.current, {
+        scale: 0,
+      }).to(portalRef.current, {
+        pin: true,
+      });
+    },
+    { scope: containerRef }
+  );
+
+  return (
+    <div className={styles.Main} ref={containerRef}>
+      <div className={styles.Portal} ref={portalRef}></div>
+    </div>
+  );
 }
