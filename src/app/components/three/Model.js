2379a1bc5e9e0981f3a7ce2c9a2aafc5b526e26f
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-07-28 15:26:31 +0200

    changes

--- src/app/components/three/Model.js
+++ src/app/components/three/Model.js
@@ -1,48 +1,13 @@
 'use client';
-import React, { useRef, useEffect, useState } from 'react';
-import { useThree, useFrame } from '@react-three/fiber';
+import React, { useRef, useEffect } from 'react';
+import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
-import {
-  useGLTF,
-  OrbitControls,
-  Environment,
-  MeshDistortMaterial,
-} from '@react-three/drei';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useGSAP } from '@gsap/react';
-
-gsap.registerPlugin(ScrollTrigger);
+import { useGLTF, OrbitControls, Environment } from '@react-three/drei';
 
 export default function Model() {
   const meshRef = useRef();
-  const { nodes } = useGLTF('/assets/neuuu.glb');
+  const { nodes } = useGLTF('/assets/glassy.glb');
   const timeRef = useRef(0);
-  const scrollProgressRef = useRef(0);
-  const [scrollDistortion, setScrollDistortion] = useState(0);
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  const calcDelta = () => {
-    const position = window.scrollY;
-    setTimeout(() => {
-      setScrollPosition(0);
-    }, 100);
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', calcDelta);
-  }, [scrollPosition]);
-
-  useGSAP(() => {
-    ScrollTrigger.create({
-      start: 0,
-      end: 'max',
-      onUpdate: (self) => {
-        scrollProgressRef.current = self.progress;
-        setScrollDistortion(self.progress * 0.4);
-      },
-    });
-  }, []);
 
   useEffect(() => {
     if (meshRef.current) {
@@ -58,17 +23,17 @@ export default function Model() {
       timeRef.current += delta;
       meshRef.current.rotation.z += 0.1 * delta;
       meshRef.current.position.y += Math.sin(timeRef.current) * 0.001;
-      meshRef.current.rotation.z = scrollProgressRef.current * Math.PI;
     }
   });
 
   return (
     <group>
-      <mesh ref={meshRef} geometry={nodes.Curve002.geometry} scale={1.13}>
-        <MeshDistortMaterial
-          distort={scrollDistortion}
-          speed={1.5}
-          color={new THREE.Color('black')}
+      <mesh ref={meshRef} geometry={nodes.Curve002.geometry} scale={3}>
+        <meshStandardMaterial
+          color={new THREE.Color('white')}
+          metalness={1}
+          roughness={0.2}
+          envMapIntensity={1.5}
         />
       </mesh>
 
